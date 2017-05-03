125299280e74e6e99aced8e40a632e038550ee45
Maslakov Denis <miel.design@gmail.com>
2017-05-03 09:26:22 +0300

    Added Redux.

---
+++ src/actions/Post.js
@@ -0,0 +1,27 @@
+import * as types from 'constants/actionTypes/PostActionTypes';
+import request from 'superagent';
+import { API_SERVER_PATH } from 'constants/blogConfig';
+
+const requestPost = (id) => ({
+  type: types.FETCH_POST_REQUEST
+});
+
+const errorPost = () => ({
+  type: types.FETCH_POST_ERROR
+});
+
+const receivePost = (response) => ({
+  type: types.FETCH_POST_SUCCESS,
+  response
+});
+
+export function fetchPost(id) {
+  return (dispatch) => {
+    dispatch(requestPost(id));
+    return request
+      .get(`${API_SERVER_PATH}/posts/${id}`)
+      .end((err, response) => {
+        err ? dispatch(errorPost()) : dispatch(receivePost(response.body));
+      });
+  };
+}
